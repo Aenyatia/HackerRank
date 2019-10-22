2a431d1b7c22a5c1e2c680e1de455b145b41364e
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 12:11:57 +0200

    Add Hourglass exercise.

---
+++ HackerRank.Domain.UnitTests/Arrays/HourglassTests.cs
@@ -0,0 +1,147 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.Arrays;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.Arrays
+{
+	public class HourglassTests
+	{
+		[Fact]
+		public void T1()
+		{
+			// arrange
+			var arr = new[]
+			{
+				new[] { -9, -9, -9, -9, -9, -9 },
+				new[] { -9, -9, -9, -9, -9, -9 },
+				new[] { -9, -9, -9, -9, -9, -9 },
+				new[] { -9, -9, -9, -9, -9, -9 },
+				new[] { -9, -9, -9, -9, -9, -9 },
+				new[] { -9, -9, -9, -9, -9, -9 }
+			};
+
+			const int expected = -63;
+
+			// act
+			var result = Hourglass.HourglassSum(arr);
+
+			// assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void T2()
+		{
+			// arrange
+			var arr = new[]
+			{
+				new[] { 0, 0, 0, 0, 0, 0 },
+				new[] { 0, 0, 0, 0, 0, 0 },
+				new[] { 0, 0, 0, 0, 0, 0 },
+				new[] { 0, 0, 0, 0, 0, 0 },
+				new[] { 0, 0, 0, 0, 0, 0 },
+				new[] { 0, 0, 0, 0, 0, 0 }
+			};
+
+			const int expected = 0;
+
+			// act
+			var result = Hourglass.HourglassSum(arr);
+
+			// assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void T3()
+		{
+			// arrange
+			var arr = new[]
+			{
+				new[] { 9, 9, 9, 9, 9, 9 },
+				new[] { 9, 9, 9, 9, 9, 9 },
+				new[] { 9, 9, 9, 9, 9, 9 },
+				new[] { 9, 9, 9, 9, 9, 9 },
+				new[] { 9, 9, 9, 9, 9, 9 },
+				new[] { 9, 9, 9, 9, 9, 9 }
+			};
+
+			const int expected = 63;
+
+			// act
+			var result = Hourglass.HourglassSum(arr);
+
+			// assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void T4()
+		{
+			// arrange
+			var arr = new[]
+			{
+				new[] { 1, 1, 1, 0, 0, 0 },
+				new[] { 0, 1, 0, 0, 0, 0 },
+				new[] { 1, 1, 1, 0, 0, 0 },
+				new[] { 0, 0, 2, 4, 4, 0 },
+				new[] { 0, 0, 0, 2, 0, 0 },
+				new[] { 0, 0, 1, 2, 4, 0 }
+			};
+
+			const int expected = 19;
+
+			// act
+			var result = Hourglass.HourglassSum(arr);
+
+			// assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void T5()
+		{
+			// arrange
+			var arr = new[]
+			{
+				new[] { 1, 1, 1, 0, 0, 0 },
+				new[] { 0, 1, 0, 0, 0, 0 },
+				new[] { 1, 1, 1, 0, 0, 0 },
+				new[] { 0, 9, 2, -4, -4, 0 },
+				new[] { 0, 0, 0, -2, 0, 0 },
+				new[] { 0, 0, -1, -2, -4, 0 }
+			};
+
+			const int expected = 13;
+
+			// act
+			var result = Hourglass.HourglassSum(arr);
+
+			// assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void T6()
+		{
+			// arrange
+			var arr = new[]
+			{
+				new[] { -9, -9, -9, 1, 1, 1 },
+				new[] { 0, -9, 0, 4, 3, 2 },
+				new[] { -9, -9, -9, 1, 2, 3 },
+				new[] { 0, 0, 8, 6, 6, 0 },
+				new[] { 0, 0, 0, -2, 0, 0 },
+				new[] { 0, 0, 1, 2, 4, 0 }
+			};
+
+			const int expected = 28;
+
+			// act
+			var result = Hourglass.HourglassSum(arr);
+
+			// assert
+			result.Should().Be(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/Arrays/Hourglass.cs
@@ -0,0 +1,32 @@
+﻿namespace HackerRank.Domain.Arrays
+{
+	public class Hourglass
+	{
+		public static int HourglassSum(int[][] arr)
+		{
+			var maxSum = -63;
+
+			for (var i = 0; i < 4; i++)
+			{
+				for (var j = 0; j < 4; j++)
+				{
+					var sum = 0;
+
+					// top 3 elements
+					sum += arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+
+					// mid element
+					sum += arr[i + 1][j + 1];
+
+					// bottom 3 elements
+					sum += arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+					if (sum > maxSum)
+						maxSum = sum;
+				}
+			}
+
+			return maxSum;
+		}
+	}
+}
