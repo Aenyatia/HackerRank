55362dbcf885c7c01f6729f12dee822fe6bdf4eb
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 13:49:49 +0200

    Add CountingValleys exercise.

---
+++ HackerRank.Domain.UnitTests/WarmUp/CountingValleysTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.WarmUp;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.WarmUp
+{
+	public class CountingValleysTests
+	{
+		[Theory]
+		[InlineData("UDDDUDUU", 8, 1)]
+		[InlineData("DDUUDDUDUUUD", 12, 2)]
+		public void T(string s, int n, int expected)
+		{
+			// act
+			var result = CountingValleys.CountValleys(n, s);
+
+			// assert
+			result.Should().Be(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/WarmUp/CountingValleys.cs
@@ -0,0 +1,27 @@
+﻿namespace HackerRank.Domain.WarmUp
+{
+	public class CountingValleys
+	{
+		public static int CountValleys(int n, string s)
+		{
+			var valleys = 0;
+			var currentHeight = 0;
+			var nextHeight = 0;
+
+			foreach (var step in s)
+			{
+				if (step == 'U')
+					nextHeight++;
+				else
+					nextHeight--;
+
+				if (currentHeight == 0 && nextHeight == -1)
+					valleys++;
+
+				currentHeight = nextHeight;
+			}
+
+			return valleys;
+		}
+	}
+}
