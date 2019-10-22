77df825935d9a0987e61ec127f2fc68c3cb0d085
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 14:26:22 +0200

    Add RansomNote exercise.

---
+++ HackerRank.Domain.UnitTests/Dictionaries/RansomNoteTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.Dictionaries;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.Dictionaries
+{
+	public class RansomNoteTests
+	{
+		[Theory]
+		[InlineData(new[] { "give", "me", "one", "grand", "today", "night" }, new[] { "give", "one", "grand", "today" }, "Yes")]
+		[InlineData(new[] { "two", "times", "three", "is", "not", "four" }, new[] { "two", "times", "two", "is", "four" }, "No")]
+		[InlineData(new[] { "ive", "got", "a", "lovely", "bunch", "of", "coconuts" }, new[] { "ive", "got", "some", "coconuts" }, "No")]
+
+		public void T(string[] magazine, string[] note, string expected)
+		{
+			// act
+			var result = RansomNote.CheckMagazine(magazine, note);
+
+			// assert
+			result.Should().BeEquivalentTo(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/Dictionaries/RansomNote.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank.Domain.Dictionaries
+{
+	public class RansomNote
+	{
+		public static string CheckMagazine(string[] magazine, string[] note)
+		{
+			var magazineWords = new Dictionary<string, int>();
+
+			// create list of unique words with their occurrences
+			foreach (var word in magazine)
+			{
+				if (magazineWords.ContainsKey(word))
+					magazineWords[word]++;
+				else
+					magazineWords.Add(word, 1);
+			}
+
+			foreach (var word in note)
+			{
+				if (!magazineWords.ContainsKey(word) ||
+					magazineWords[word] == 0)
+					return "NO";
+
+				magazineWords[word]--;
+			}
+
+			return "YES";
+		}
+	}
+}
