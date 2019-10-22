665a4e4a191aeda940df8924c9678c562025d1c2
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 12:17:33 +0200

    Add MinimumSwaps exercise.

---
+++ HackerRank.Domain.UnitTests/Arrays/MinimumSwapsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.Arrays;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.Arrays
+{
+	public class MinimumSwapsTests
+	{
+		[Theory]
+		[InlineData(new[] { 4, 3, 1, 2 }, 3)]
+		[InlineData(new[] { 2, 3, 4, 1, 5 }, 3)]
+		[InlineData(new[] { 1, 3, 5, 2, 4, 6, 7 }, 3)]
+		public void T(int[] arr, int expected)
+		{
+			// act
+			var result = MinimumSwaps.MinSwaps(arr);
+
+			// result
+			result.Should().Be(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/Arrays/MinimumSwaps.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank.Domain.Arrays
+{
+	public class MinimumSwaps
+	{
+		public static int MinSwaps(int[] arr)
+		{
+			var minSwaps = 0;
+
+			var index = 0;
+			while (index < arr.Length)
+			{
+				if (arr[index] != index + 1)
+				{
+					Swap(arr, index, arr[index] - 1);
+					minSwaps++;
+				}
+				else
+				{
+					index++;
+				}
+			}
+
+			return minSwaps;
+		}
+
+		private static void Swap(IList<int> array, int firstIndex, int secondIndex)
+		{
+			var temp = array[firstIndex];
+
+			array[firstIndex] = array[secondIndex];
+			array[secondIndex] = temp;
+		}
+	}
+}
