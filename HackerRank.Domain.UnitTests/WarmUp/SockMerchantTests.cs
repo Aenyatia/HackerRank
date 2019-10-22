81e309cc8d7895d2314b54bd549a576d0e742d9c
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 12:46:22 +0200

    Add SockMerchant exercise.

---
+++ HackerRank.Domain.UnitTests/WarmUp/SockMerchantTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.WarmUp;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.WarmUp
+{
+	public class SockMerchantTests
+	{
+		[Theory]
+		[InlineData(new[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 }, 3)]
+		[InlineData(new[] { 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 }, 4)]
+		public void T1(int[] ar, int expected)
+		{
+			// act
+			var result = SockMerchant.SockMer(ar);
+
+			// assert
+			result.Should().Be(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/WarmUp/SockMerchant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Domain.WarmUp
+{
+	public class SockMerchant
+	{
+		public static int SockMer(int[] ar)
+		{
+			var socks = new Dictionary<int, int>();
+
+			foreach (var sock in ar)
+			{
+				if (socks.ContainsKey(sock))
+					socks[sock]++;
+				else
+					socks.Add(sock, 1);
+			}
+
+			return socks.Sum(sockColor => sockColor.Value / 2);
+		}
+	}
+}
