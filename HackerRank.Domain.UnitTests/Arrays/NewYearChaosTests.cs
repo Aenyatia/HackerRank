10d463345404fdf2949be6f85ac1a61ccc3d97c5
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 12:20:17 +0200

    Add NewYearChaos exercise.

---
+++ HackerRank.Domain.UnitTests/Arrays/NewYearChaosTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.Arrays;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.Arrays
+{
+	public class NewYearChaosTests
+	{
+		[Theory]
+		[InlineData(new[] { 2, 1, 5, 3, 4 }, "3")]
+		[InlineData(new[] { 2, 5, 1, 3, 4 }, "Too chaotic")]
+		[InlineData(new[] { 5, 1, 2, 3, 7, 8, 6, 4 }, "Too chaotic")]
+		[InlineData(new[] { 1, 2, 5, 3, 7, 8, 6, 4 }, "7")]
+		[InlineData(new[] { 1, 2, 5, 3, 4, 7, 8, 6 }, "4")]
+		public void T(int[] q, string expected)
+		{
+			// act
+			var result = NewYearChaos.MinimumBribes(q);
+
+			// result
+			result.Should().Be(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/Arrays/NewYearChaos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HackerRank.Domain.Arrays
+{
+	public class NewYearChaos
+	{
+		public static string MinimumBribes(int[] q)
+		{
+			var bribes = 0;
+
+			for (var i = q.Length - 1; i >= 0; i--)
+			{
+				// if someone bribed more then two people, print 'Too chaotic'
+				if (q[i] - (i + 1) > 2)
+				{
+					return "Too chaotic";
+				}
+
+				// if number on left side from q[i] is grater then q[i] that means we have a bribe
+				// [ 1, 2, 5, 3, 7, 8, 6, 4 ] means 4 was bribed by 5, 7, 8, 6
+				//									6 was bribed by 7, 8
+				//									3 was bribed by 5
+				for (var j = Math.Max(0, q[i] - 2); j < i; j++)
+				{
+					if (q[j] > q[i])
+						bribes++;
+				}
+			}
+
+			return bribes.ToString();
+		}
+	}
+}
