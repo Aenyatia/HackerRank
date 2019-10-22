4afc6744be10230a612a1331529214b4a5c35843
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 12:15:11 +0200

    Add ArrayManipulation exercise.

---
+++ HackerRank.Domain.UnitTests/Arrays/ArrayManipulationTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.Arrays;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.Arrays
+{
+	public class ArrayManipulationTests
+	{
+		[Fact]
+		public void T1()
+		{
+			// arrange
+			const int n = 5;
+			var queries = new[]
+			{
+				new []{ 1, 2, 100 },
+				new []{ 2, 5, 100 },
+				new []{ 3, 4, 100 }
+			};
+
+			const int expected = 200;
+
+			// act
+			var result = ArrayManipulation.ArrManipulation(n, queries);
+
+			// assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void T2()
+		{
+			// arrange
+			const int n = 10;
+			var queries = new[]
+			{
+				new []{ 1, 5, 3 },
+				new []{ 4, 8, 7 },
+				new []{ 6, 9, 1 }
+			};
+
+			const int expected = 10;
+
+			// act
+			var result = ArrayManipulation.ArrManipulation(n, queries);
+
+			// assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void T3()
+		{
+			// arrange
+			const int n = 10;
+			var queries = new[]
+			{
+				new []{ 2, 6, 8 },
+				new []{ 3, 5, 7 },
+				new []{ 1, 8, 1 },
+				new []{ 5, 9, 15 }
+			};
+
+			const int expected = 31;
+
+			// act
+			var result = ArrayManipulation.ArrManipulation(n, queries);
+
+			// assert
+			result.Should().Be(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/Arrays/ArrayManipulation.cs
@@ -0,0 +1,39 @@
+﻿namespace HackerRank.Domain.Arrays
+{
+	public class ArrayManipulation
+	{
+		public static long ArrManipulation(int n, int[][] queries)
+		{
+			var resultArray = new long[n + 1];
+
+			foreach (var query in queries)
+			{
+				long startIndex = query[0];
+				long endIndex = query[1];
+				long numberToSum = query[2];
+
+				resultArray[startIndex] += numberToSum;
+
+				if (endIndex + 1 <= n)
+				{
+					resultArray[endIndex + 1] -= numberToSum;
+				}
+			}
+
+			long maxValue = 0;
+			long prefixSum = 0;
+
+			for (var i = 1; i < resultArray.Length; i++)
+			{
+				prefixSum += resultArray[i];
+
+				if (prefixSum > maxValue)
+				{
+					maxValue = prefixSum;
+				}
+			}
+
+			return maxValue;
+		}
+	}
+}
