6016d9ec2a5d532f38a9750dec3a78dbcecc9f46
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 12:16:26 +0200

    Add LeftRotation exercise.

---
+++ HackerRank.Domain.UnitTests/Arrays/LeftRotationTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.Arrays;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.Arrays
+{
+	public class LeftRotationTests
+	{
+		[Theory]
+		[InlineData(new[] { 1, 2, 3, 4, 5 }, 4, new[] { 5, 1, 2, 3, 4 })]
+		[InlineData(new[] { 41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51 }, 10, new[] { 77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77 })]
+		[InlineData(new[] { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97 }, 13, new[] { 87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60 })]
+		public void T(int[] a, int d, int[] expected)
+		{
+			// act
+			var result = LeftRotation.RotLeft(a, d);
+
+			// assert
+			result.Should().BeEquivalentTo(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/Arrays/LeftRotation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HackerRank.Domain.Arrays
+{
+	public class LeftRotation
+	{
+		public static int[] RotLeft(int[] a, int d)
+		{
+			var shift = d % a.Length;
+			var shiftedArray = new int[a.Length];
+
+			Array.Copy(a, shift, shiftedArray, 0, a.Length - shift);
+			Array.Copy(a, 0, shiftedArray, a.Length - shift, shift);
+
+			return shiftedArray;
+		}
+	}
+}
