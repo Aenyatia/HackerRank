d879d191e488dcfec2ed1e5300b2aae654e10964
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 13:54:35 +0200

    Add JumpingOnTheClouds exercise.

---
+++ HackerRank.Domain.UnitTests/WarmUp/JumpingOnTheCloudsTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.WarmUp;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.WarmUp
+{
+	public class JumpingOnTheCloudsTests
+	{
+		[Theory]
+		[InlineData(new[] { 0, 0, 1, 0, 0, 1, 0 }, 4)]
+		[InlineData(new[] { 0, 0, 0, 1, 0, 0 }, 3)]
+		public void T(int[] c, int expected)
+		{
+			// act
+			var result = JumpingOnTheClouds.JumpingOnClouds(c);
+
+			// assert
+			result.Should().Be(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/WarmUp/JumpingOnTheClouds.cs
@@ -0,0 +1,24 @@
+﻿namespace HackerRank.Domain.WarmUp
+{
+	public class JumpingOnTheClouds
+	{
+		public static int JumpingOnClouds(int[] c)
+		{
+			var jumps = 0;
+			var i = 0;
+
+			while (i < c.Length - 1)
+			{
+				// if 2nd field forward is empty jump there, else do one step
+				if (i + 2 < c.Length && c[i + 2] != 1)
+					i += 2;
+				else
+					i++;
+
+				jumps++;
+			}
+
+			return jumps;
+		}
+	}
+}
