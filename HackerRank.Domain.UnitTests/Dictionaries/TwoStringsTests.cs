dd0326bf28dde5eaef824227006832651a76a81e
Aenyatia Nirvalen <aenyatia@gmail.com>
2019-10-22 14:01:41 +0200

    Add TwoStrings exercise.

---
+++ HackerRank.Domain.UnitTests/Dictionaries/TwoStringsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using HackerRank.Domain.Dictionaries;
+using Xunit;
+
+namespace HackerRank.Domain.UnitTests.Dictionaries
+{
+	public class TwoStringsTests
+	{
+		[Theory]
+		[InlineData("hello", "world", "YES")]
+		[InlineData("hi", "world", "NO")]
+		[InlineData("wouldyoulikefries", "abcabcabcabcabcabc", "NO")]
+		[InlineData("hackerrankcommunity", "cdecdecdecde", "YES")]
+		[InlineData("jackandjill", "wentupthehill", "YES")]
+		[InlineData("writetoyourparents", "fghmqzldbc", "NO")]
+		public void T(string s1, string s2, string expected)
+		{
+			// act
+			var result = TwoStrings.TwoStr(s1, s2);
+
+			// assert
+			result.Should().BeEquivalentTo(expected);
+		}
+	}
+}
---
+++ HackerRank.Domain/Dictionaries/TwoStrings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Domain.Dictionaries
+{
+	public class TwoStrings
+	{
+		public static string TwoStr(string s1, string s2)
+		{
+			var uniqueChars = new HashSet<char>();
+
+			foreach (var c in s1)
+				uniqueChars.Add(c);
+
+			return s2.Any(c => uniqueChars.Contains(c)) ? "YES" : "NO";
+		}
+	}
+}
